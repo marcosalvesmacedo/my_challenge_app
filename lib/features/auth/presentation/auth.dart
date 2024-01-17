a06b8825cf85016639f9d2c436af0bd548d6a3bc
marcosalvesmacedo <marcosalvesmacedo@hotmail.com>
2024-01-17 15:43:01 -0300

    Criação da estrutura da pasta de autenticação

---
+++ lib/features/auth/presentation/auth.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class Auth extends StatefulWidget {
+  const Auth({super.key, required this.title});
+  final String title;
+
+  @override
+  State<Auth> createState() => _AuthState();
+}
+
+class _AuthState extends State<Auth> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text('My Challenge App'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
